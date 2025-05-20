5ee91b34dfa30e60dcb4d0787a7004fd30cd46c7
Mithun James <drtechie@users.noreply.github.com>
2025-05-20 12:29:56 +0530

    Ci setup (#30)
    * Add CICD setup, add env variables
    
    * fix: add node sass, fix scripts
    
    * fix: update TM API
    
    * fix: delete env file
    
    * fix: package script for building
    
    * fix: copy command
    
    * fix: update GitHub actions
    
    * fix: run build-ci instead of build
    
    ---------
    
    Co-authored-by: Mithun James <1007084+drtechie@users.noreply.github.com>

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -24,60 +24,22 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-
-const commonIP = 'https://amritwprdev.piramalswasthya.org/';
-const IP1097 = 'https://amritwprdev.piramalswasthya.org/';
-const telephonyServerIP = 'https://uatcz.piramalswasthya.org/';
-const adminIP = 'https://amritwprdev.piramalswasthya.org/';
-const IP104 = 'https://amritwprdev.piramalswasthya.org/';
-const mmuIP = 'https://amritwprdev.piramalswasthya.org/';
-const tmIP = 'https://amritwprdev.piramalswasthya.org/';
-const FHIRIP = "https://amritwprdev.piramalswasthya.org";
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class ConfigService {
 
-    // private _commonBaseURL: String = "http://deviemr.piramalswasthya.org:8080/commonapi-v1.1/";
-    // private _helpline1097BaseURL: String = "http://deviemr.piramalswasthya.org:8080/1097api-v1.0/";
-    // private _helpline104BaseURL: String = "http://deviemr.piramalswasthya.org:8080/104api-v1.0/";
-    // private _adminBaseURL: String = "http://deviemr.piramalswasthya.org:8080/adminapi-v1.0/";
-    // private _telephonyServerURL: String = "http://helplines.piramalswasthya.org/"; 
+    private _helpline1097BaseURL: string = environment.ip1097;
+    private _telephonyServerURL: string = environment.telephoneServer;
+    private _commonBaseUrlForLicense: string = environment.commonAPI;
+    private _commonBaseURL: string = environment.commonAPI;
+    private _opencommonBaseURL: string = environment.commonAPI;
+    private _helpline104BaseURL: string = environment.ip104;
+    private _adminBaseURL: string = environment.adminAPI;
+    private _mmuBaseURL: string = environment.adminAPI;
+    private _tmBaseURL: string = environment.tmAPI;
+    private _fhirBaseURL = environment.fhirAPI;
 
-
-    private _helpline1097BaseURL: string = `${IP1097}1097api-v1.0/`;
-    private _telephonyServerURL: string = `${telephonyServerIP}`;
-    private _commonBaseUrlForLicense: string = `${commonIP}commonapi-v1.0/`;
-    private _commonBaseURL: string = `${commonIP}commonapi-v1.0/`;
-    private _opencommonBaseURL: string = `${commonIP}commonapi-v1.0/`;
-    private _helpline104BaseURL: string = `${IP104}104api-v1.0/`;
-    private _adminBaseURL: string = `${adminIP}adminapi-v1.0/`;
-    private _mmuBaseURL: string = `${mmuIP}mmuapi-v1.0/`;
-    private _tmBaseURL: string = `${tmIP}tmapi-v1.0/`;
-    private _fhirBaseURL = `${FHIRIP}/fhirapi-v1.0/`;
-    // private czentrics ='http://10.208.122.99/';
-    // private _commonBaseURL: String = `http://localhost:8080/`;
-    // private _helpline104BaseURL: String = `http://localhost:8080/`;
-    // private _opencommonBaseURL: String = `http://localhost:8080/`;
-    // private _commonBaseUrlForLicense: String = `http://localhost:8080/`;
-
-    // private _commonBaseURL: String = "http://amritwprdev.piramalswasthya.org:8080/apiman-gateway/IEMR/Common/1.0/";
-    // private _opencommonBaseURL: String="http://amritwprdev.piramalswasthya.org:8080/apiman-gateway/IEMR/Common/open/";
-    //   private _helpline104BaseURL: String = "http://amritwprdev.piramalswasthya.org:8080/apiman-gateway/IEMR/104/1.0/";
-
-    // 10.208.122.38
-    //NOTE: Socket URL is mentioned in Socket Service has  as it is giving cyclic dependency error if given here
-
-    //    private _commonBaseURL: String = "http://l-442002723.wipro.com:8080/commonapi-v1.1/";
-    // private _helpline104BaseURL: String = "http://l-285002006.wipro.com:8080/104api-v1.0/";
-    // private _helpline1097BaseURL: String = "http://l-285002006.wipro.com:8080/helpline1097APIV1/";
-    // private _adminBaseURL: String = "http://l-285002006.wipro.com:8080/adminapi-v1.0/"; 
-    // private _telephonyServerURL: String = "http://10.208.122.99/";  
-
-    // private _commonBaseURL: String = "http://localhost:8080/commonapi-v1.1/";
-    // private _helpline104BaseURL: String = "http://localhost:8080/104api-v1.0/";
-    // private _helpline1097BaseURL: String = "http://localhost:8080/helpline1097APIV1/";
-    // private _adminBaseURL: String = "http://localhost:8080/adminapi-v1.0/"; 
-    // private _telephonyServerURL: String = "http://10.208.122.99/";  
 
     public defaultWrapupTime: any = 30;
 
@@ -111,9 +73,4 @@ export class ConfigService {
     getFHIRBaseURL() {
         return this._fhirBaseURL;
     }
-    // getczentricsURL(){
-    //     return this.czentrics;
-    // }
-    
-
 }
